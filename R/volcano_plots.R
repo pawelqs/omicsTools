4637a0cc961966be0fc4cbbf56c2a429ab683abd
Paweł <38688203+pawel125@users.noreply.github.com>
2022-05-19 14:27:48 +0200

    Volcano plots updated

--- R/volcano_plots.R
+++ R/volcano_plots.R
@@ -5,7 +5,7 @@
 plot_volcano <- function(df, p_adj_col = "padj", lfc_col = "log2FoldChange",
                          label_col = "mgi_symbol",
                          p_adj_treshold = 0.05, lfc_treshold = 1,
-                         labels = 20) {
+                         labels_n = 20, labels = NULL) {
 
   df <- mutate(df,
     significant = !!sym(p_adj_col) < p_adj_treshold,
@@ -36,12 +36,14 @@ plot_volcano <- function(df, p_adj_col = "padj", lfc_col = "log2FoldChange",
       n = replace_na(n, 0),
       text = str_c(change, ": ", n))
 
-  labels <- if (is.numeric(labels)) {
+  labels_data <- list()
+  labels_data$empty <- slice(df, 0)
+  labels_data$top <- if (labels_n > 0) {
     n_up <- filter(numbers, change == "up")$n
     n_down <- filter(numbers, change == "down")$n
     n_changed <- n_up + n_down
-    label_n_up <- if (n_changed > 0) round((n_up * labels) / n_changed) else 0
-    label_n_down <- if (n_changed > 0) round((n_down * labels) / n_changed) else 0
+    label_n_up <- if (n_changed > 0) round((n_up * labels_n) / n_changed) else 0
+    label_n_down <- if (n_changed > 0) round((n_down * labels_n) / n_changed) else 0
 
     labels_up <- df %>%
       filter(change == "up") %>%
@@ -52,11 +54,11 @@ plot_volcano <- function(df, p_adj_col = "padj", lfc_col = "log2FoldChange",
       arrange(!!sym(p_adj_col)) %>%
       slice(1:label_n_down)
     bind_rows(labels_up, labels_down)
-  } else if (is.character(labels)) {
-    filter(df, !!sym(label_col) %in% labels)
-  } else {
-    slice(df, 0)
   }
+  labels_data$chosen <- if (is.character(labels)) {
+    filter(df, !!sym(label_col) %in% labels)
+  }
+  labels_data <- bind_rows(labels_data)
 
   ggplot(df, aes(!!sym(lfc_col), -log10(!!sym(p_adj_col)), color = change)) +
     geom_point() +
@@ -65,7 +67,7 @@ plot_volcano <- function(df, p_adj_col = "padj", lfc_col = "log2FoldChange",
       breaks = c("down", "not sig.", "up")
     ) +
     geom_label_repel(aes(label = !!sym(label_col)),
-      data = labels, show.legend = FALSE, max.overlaps = 30
+      data = labels_data, show.legend = FALSE, max.overlaps = 30
     ) +
     xlim(-x_max, x_max) +
     geom_hline(yintercept = -log10(p_adj_treshold), linetype = "dashed") +
@@ -74,5 +76,6 @@ plot_volcano <- function(df, p_adj_col = "padj", lfc_col = "log2FoldChange",
     geom_label(aes(x, y, vjust = y_just, hjust = x_just, label = text),
                data = numbers, color = "black", size = 5
     ) +
+    theme_minimal() +
     theme(legend.position = "bottom")
 }
